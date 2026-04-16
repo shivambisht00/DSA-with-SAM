d355e86826b8aa860dd0f7038ccd96c3fd4510a1
Shivam Bisht <8171659929sb@gmail.com>
2026-04-16 21:55:51 +0530

    Add string reversal program in reverse.cpp
    Implement a simple program to reverse a string input by the user.

---
+++ reverse.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str;
+    cout << "Enter a string: ";
+    cin >> str;
+
+    int n = str.length();
+
+    for(int i = n - 1; i >= 0; i--) {
+        cout << str[i];
+    }
+
+    return 0;
+}

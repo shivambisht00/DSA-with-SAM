97a04430247d8cdad50ed0c6874e75389f83699a
Shivam Bisht <8171659929sb@gmail.com>
2026-05-06 19:55:35 +0530

    Refactor comments in Reverse_integer.cpp
    Updated comments for clarity and consistency.

---
+++ array/Reverse_integer.cpp
@@ -0,0 +1,28 @@
+// leet code 7. Reverse Integer
+// Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+// Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+ 
+
+// Example 1:
+
+// Input: x = 123
+// Output: 321
+
+class Solution {
+public:
+    int reverse(int x) {
+        int rev = 0;
+
+        while(x != 0) {
+            int digit = x % 10;
+            if(rev > INT_MAX/10 || rev < INT_MIN/10) {
+                return 0;
+            }
+            rev = rev * 10 + digit;
+            x = x / 10;
+        }
+        return rev;
+    }
+};

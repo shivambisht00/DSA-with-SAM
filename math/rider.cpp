c13a2d85e373ccd3ad1e577885707a8fa7293f29
Shivam Bisht <8171659929sb@gmail.com>
2026-06-19 22:35:00 +0530

    Implement largestAltitude function in rider.cpp

---
+++ math/rider.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int max = 0;
+        int   sum = 0;
+        for(int i = 0 ; i < gain.size();i++){
+          sum +=  gain[i];
+            if(sum> max){
+                max = sum;
+            }
+        }
+        return max;
+    }
+};
